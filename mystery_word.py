7d8efdd9f9a9564cfcc0a4eb6995760ed5f79af8
jocelyninlay <inlay.j@gmail.com>
2022-02-25 12:33:26 -0500

    randomly selects word and tells how many letters long

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,22 @@
-def play_game():
-    pass
+import random
 
+from pyparsing import Word
+
+def play_game():
+#    theirname = input("What is your name?")
+#    print("Good luck",theirname)
+
+    with open("test-word.txt") as test_word:
+        words_list = test_word.read()
+        words = list(map(str, words_list.split()))
+        print("Your word is", len(random.choice(words)), "letters long.")
+
+
+
+    # guesses=[]
+    # their_guess = input("Guess the word one letter at a time")
+    # guess_list = their_guess.split()
+    # print('guess:', guess_list)
 
 if __name__ == "__main__":
     play_game()
