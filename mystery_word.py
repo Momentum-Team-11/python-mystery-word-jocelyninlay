92ae8e1b89dfab9b5843c4ae21bd3b38d2b6a0a8
Jocelyn <inlay.j@gmail.com>
2022-02-25 21:24:37 -0500

    added some stuff that doesnt work but is maybe on the right path

--- mystery_word.py
+++ mystery_word.py
@@ -10,19 +10,21 @@ def play_game():
         words = list(map(str, words_list.split()))
         global the_word 
         the_word = random.choice(words)
-        count = 8
+        count = 5
         print("Your word is", len(the_word), "letters long.")   
+  guesslist = [] 
   while count > 0:
-      guesses = input("Guess one letter at a time. Make your guess:") 
+      current_guess = input("Guess one letter at a time. Make your guess:")
       count = count - 1
       if count < 1:
                 print("Sorry, game over")
                 exit()
-      print([letter if letter in guesses else "_" for letter in the_word])
+      print([letter if letter in current_guess else "_" for letter in the_word], "you have", count,"guesses left.")
+      if current_guess in the_word:
+       guesslist.append(current_guess)
 
-
-
-
+       
+    #    print(''.join(guesslist))
         # def display_letter(letter, guesses):
         #     if letter in guesses:
         #         return letter
